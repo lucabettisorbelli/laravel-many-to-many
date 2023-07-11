f420a9b1f55c7193429f83aff6304dc9f84a54a1
luca betti sorbelli <luca betti sorbelli>
2023-07-11 15:49:46 +0200

    fix

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,1 +1,21 @@
-ciao
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container-fluid mt-4">
+    <div class="row justify-content-between">
+        @foreach ($projects as $project)
+            <div class="card p-0 mb-4" style="width: 18rem;">
+                <img class="card-img-top" src="{{ $project->image }}" alt="{{ $project->title }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $project->title }}</h5>
+                    <a href="{{ route("admin.project.show", $project) }}" class="btn btn-primary">View details</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
+
+
+@endsection
